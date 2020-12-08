a3d8af3bff841b50612a66bce493e1e2b189b4af
Doktor-Doom <andre.o.martin@gmail.com>
2020-12-07 18:36:06 -0800

    stuff and things

---
+++ handlers/404.js
@@ -0,0 +1,4 @@
+function notFound(req, res){
+  res.status(404).send('not found');
+}
+module.exports = notFound;
---
+++ server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express'); 
+const app = express();
+
+const notFoundHandler = require('./handlers/404');
+const errorHandler = requrie('./handlers/500');
+
+app.get('/', renderHome);
+app.get('/data', renderData);
+app.get('/bad', (req, res, next) => {
+  next('ruh-roh');
+});
+app.use('*', notFoundHandler);
+app.use(errorHandler);
+
+function renderHome(req, res){
+  const outputObj = {
+    10: "even",
+    5: "odd",
+    "time": new Date()
+  }
+  res.status(200).json(outputObj);
+}
+function start(port) {
+  app.listen(port, () => console.log(`server listen ${port}`)); 
+}
+module.exports = {
+  app: app,
+  start: start
+}
